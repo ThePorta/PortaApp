4fe4f48a6abee3713da41f67a757727a9552b79c
goulashlala <goulashlala@example.com>
2023-09-23 16:58:39 -0400

    add bootstrap style

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import '../styles/globals.css';
 import '@rainbow-me/rainbowkit/styles.css';
+import "bootstrap/dist/css/bootstrap.min.css"
 import { getDefaultWallets, RainbowKitProvider } from '@rainbow-me/rainbowkit';
 import type { AppProps } from 'next/app';
 import { configureChains, createConfig, WagmiConfig } from 'wagmi';
